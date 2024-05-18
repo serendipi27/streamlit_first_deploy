b2470bc0fecb66b491c909689758122dcd3f5dd8
serendipi27 <serendipi27@gmail.com>
2024-05-18 21:30:20 +0900

    update app.py

--- app.py
+++ app.py
@@ -6,4 +6,30 @@ import seaborn as sns
 import plotly.express as px
 import streamlit as st
 
-df = px.dat
+df = px.data.experiment()
+
+def bar_chart(df):
+    bar_data = df.pivot_table(index='group', values='experiment_3', aggfunc='mean').reset_index()
+    st.bar_chart(bar_data, x='group', y='experiment_3', color='group')
+
+def box_chart(df):
+    plt.figure(figsize=(10, 8))
+    plt.title('Box plot of experiment_2 by gender')
+    sns.boxplot(df, x='gender', y='experiment_2')
+    st.pyplot(plt)
+
+def violine_chart(df):
+    plt.figure(figsize=(10, 8))
+    plt.title('Violine plot of experiment_1 by gender')
+    sns.violinplot(df, x='gender', y='experiment_1')
+    st.pyplot(plt)
+
+select_chart = st.selectbox("차트를 선택하세요", ['BarChart', 'BoxChart', 'ViolineChart'])
+
+if select_chart == 'BarChart':
+    bar_chart(df)
+elif select_chart == 'BoxChart':
+    box_chart(df)
+else:
+    violine_chart(df)
+    
