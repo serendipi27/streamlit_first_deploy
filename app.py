f81ac7f0b93ae4437c5c826c0c74fe67e00d3ecc
serendipi27 <serendipi27@gmail.com>
2024-05-18 21:07:43 +0900

    updates app.py

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 import streamlit as st
 
-# 페이지 선택
-# page = st.sidebar.selectbox('Select your page', ['Home', 'Data Visualization', 'About'])
-page = st.sidebar.radio('Select your page', ['Home', 'Data Visualization', 'About'])
+df = px.data.experiment()
 
-if page == 'Home':
-    st.title('Home Page')
-    st.write('Welcome to the Home Page!')
-elif page == 'Data Visualization':
-    st.title('Data Visualization')
-    st.write('Here you can see interactive charts and data insights.')
-elif page == 'About':
-    st.title('About')
-    st.write('This is an app built with Streamlit to demonstrate multi-page functionality.')
+def bar_chart(df):
+    bar_data = df.pivot_table(index='group', values='experiment_3', aggfunc='mean').reset_index()
+    st.bar_chart(bar_data, x='group', y='experiment_3', color='group')
+
+def box_chart(df):
+    plt.figure(figsize=(10, 8))
+    plt.title('Box plot of experiment_2 by gender')
+    sns.boxplot(df, x='gender', y='experiment_2')
+    st.pyplot(plt)
+
+def violine_chart(df):
+    plt.figure(figsize=(10, 8))
+    plt.title('Violine plot of experiment_1 by gender')
+    sns.violinplot(df, x='gender', y='experiment_1')
+    st.pyplot(plt)
+
+select_chart = st.selectbox("차트를 선택하세요", ['BarChart', 'BoxChart', 'ViolineChart'])
+
+if select_chart == 'BarChart':
+    bar_chart(df)
+elif select_chart == 'BoxChart':
+    box_chart(df)
+else:
+    violine_chart(df)
+    
